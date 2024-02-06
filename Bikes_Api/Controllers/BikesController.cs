295e011d91cc086d464ef3233259092f9c18bbd8
Dinesh <dineshkumarkarikalan31@gmail.com>
2024-02-06 08:42:55 +0530

    Added Bike controller

---
+++ Bikes_Api/Controllers/BikesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bikes_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BikesController : ControllerBase
+    {
+    }
+}
